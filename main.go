17a7db809a2cdbb57c5a9dba4d2a5d0f010e44e9
Heier <alexander@engebrigtsenheier.no>
2024-09-10 18:47:04 +0200

    ADD: Basic go bot upset

--- main.go
+++ main.go
@@ -1,9 +1,118 @@
 package faceitbot
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
+// Variables to manage command-line flags.
+var GuildIDflag = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+var RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+
+// Discord session
+var dg *discordgo.Session
+
+// Discord commands definition
+var commands = []*discordgo.ApplicationCommand{
+	{
+		Name:        "faceit",
+		Description: "it just like faceitfinder.com",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "steam url",
+				Description: "the full steam url of the person you want to look up",
+				Required:    true,
+			},
+		},
+	},
+}
+
+var commandHandlers = map[string]func(dg *discordgo.Session, i *discordgo.InteractionCreate){
+	"faceit": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "This feature is under development!",
+			},
+		})
+		if err != nil {
+			log.Printf("Error responding to interaction: %v", err)
+		}
+	},
+}
+
+func init() {
+	flag.Parse()
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatalf("Error loading .env file: %v", envErr)
+	}
+
+	BotToken := os.Getenv("DISCORD_API")
+
+	var err error
+	dg, err = discordgo.New("Bot " + BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
 func main() {
-	log.Println("test")
+
+	dg.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+
+	err := dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	log.Println("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := dg.ApplicationCommandCreate(dg.State.User.ID, *GuildIDflag, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	defer dg.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	if *RemoveCommands {
+		log.Println("Removing commands...")
+
+		registeredCommands, err := dg.ApplicationCommands(dg.State.User.ID, *GuildIDflag)
+		if err != nil {
+			log.Fatalf("Could not fetch registered commands: %v", err)
+		}
+
+		for _, v := range registeredCommands {
+			err := dg.ApplicationCommandDelete(dg.State.User.ID, *GuildIDflag, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
 }
